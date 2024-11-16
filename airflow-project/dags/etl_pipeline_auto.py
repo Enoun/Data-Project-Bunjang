ea187bb359ed11455cce4bac13e646ee3b63c482
Enoun <tjd123120@gmail.com>
2024-11-17 01:37:37 +0900

    크롤링파일 수정중

--- airflow-project/dags/etl_pipeline_auto.py
+++ airflow-project/dags/etl_pipeline_auto.py
@@ -1,9 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
-import os
 
 # 기본 설정
 default_args = {
@@ -15,12 +13,14 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+venv_activated = "/Users/data-project/data-pipeline-project/.venv/bin/activate"
+
 # DAG 정의
 with DAG(
     'data_pipeline_etl',
     default_args = default_args,
     description = 'An ETL pipeline with HDFS and Spark',
-    schedule_interval = timedelta(hours=8),
+    schedule_interval = "0 7, 15, 23 * * *", #매일 7시, 15시, 23시
     start_date = days_ago(1),
     catchup = False,
 ) as dag:
@@ -28,23 +28,20 @@ with DAG(
     # 1. 데이터 수집
     collect_data = BashOperator(
         task_id = 'collect_data',
-        bash_command = 'python /Users/data-project/data-pipeline-project/product_data_scraper.py',
+        bash_command = f'source {venv_activated} && python /Users/data-project/data-pipeline-project/product_data_scraper.py',
     )
 
     # HDFS에 업로드
-    def upload_to_hdfs():
-        os.system("hdfs dfs -put /local.data/path.csv /user/dataPipeline/collectedData")
-
-    upload_data = PythonOperator(
+    upload_to_hdfs = BashOperator(
         task_id = 'upload_to_hdfs',
-        python_callable = upload_to_hdfs,
+        python_callable = 'python /Users/data-project/data-pipeline-project/upload-to-hdfs/upload_to_hdfs.py'
     )
 
     # 3. spark 전처리 작업
     spark_processing = BashOperator(
         task_id = 'spark_processing',
-        bash_command = 'spark-submit --master local /Users/data-project/data-pipeline-project/etl-saprk/etl_pipeline.py',
+        bash_command = 'python //Users/data-project/data-pipeline-project/etl-saprk/etl_pipeline.py'
     )
 
     # 작업 순서 정의
-    collect_data >> upload_data >> spark_processing
+    collect_data >> upload_to_hdfs >> spark_processing

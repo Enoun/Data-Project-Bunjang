d701072dd3328dd1e8f1dc25fab54421e067d403
Enoun <tjd123120@gmail.com>
2025-01-20 15:49:39 +0900

    파일 이름 및 구조 정리

--- airflow-project/shared/dags/etl_pipeline_auto.py
+++ airflow-project/shared/dags/etl_pipeline_auto.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
-from pytz import timezone
 
 # 기본 설정
 default_args = {
@@ -20,9 +19,9 @@ with DAG(
         dag_id = 'data_pipeline_etl',
         default_args=default_args,
         description='An ETL pipeline with HDFS and Spark',
-        # schedule_interval="@once",  # 한 번만 실행 (테스트 용)
+        schedule_interval="@once",  # 한 번만 실행 (테스트 용)
         start_date=days_ago(1),
-        schedule_interval='30 10,22 * * *',
+        # schedule_interval='30 10,22 * * *',
         catchup=False,
         max_active_runs=1,
 ) as dag:
@@ -48,7 +47,7 @@ with DAG(
             export HADOOP_HOME=/usr/local/hadoop;
             export JAVA_HOME=/usr/lib/jvm/default-java;
             export PATH=$SPARK_HOME/bin:$SPARK_HOME/sbin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH;
-            spark-submit --master local /opt/***/data-pipeline-project/etl-spark/etl_pipeline.py;
+            spark-submit --master local /opt/***/data-pipeline-project/etl-spark/spark_etl.py;
         """
     )
 
